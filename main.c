949144a69d168472e768ed62162e1a8c4b67e17c
jhfawk <150794095+jhfawk@users.noreply.github.com>
2025-02-13 19:56:28 +0300

    Add files via upload
    add sound effects and chaos function

--- main.c
+++ main.c
@@ -8,7 +8,13 @@
 #define START_CAP 5 
 #define RAYS_AMOUNT 60
 #define RADIUS 25
-#define SELECTED_CIRCLE_COLOR RED
+#define MAIN_COLOR RAYWHITE
+#define SELECTED_CIRCLE_COLOR LIGHTGRAY
+#define BACKCOLOR BLACK
+#define SELECTED_CIRCLE_RADIUS 30
+#define MAX_RANDOM_SPEED 30
+#define BOUNCE_COEFF 0.8
+#define RAY_LEN (sqrt(WIDTH * WIDTH + HEIGHT * HEIGHT))
 
 struct circle
 {
@@ -33,13 +39,16 @@ void free_circles_if_pressed(circle_array* arr);
 void draw_ray(Vector2 start_point, double len, double angle, const circle_array* col_objs, Color color);
 void draw_rays(Vector2 start_point, int amount_rays, const circle_array* c, Color color);
 void add_circle(circle_array* array, Vector2 point, float radius, Color color);
-void check_collision_w_border(circle* c);
-void handle_physic(circle_array* arr, circle* selected_circle);
+void check_collision_w_border(circle* c, Sound* s);
+void handle_physic(circle_array* arr, circle* selected_circle, Sound* s);
 void handle_position(circle_array* arr);
-void set_new_speed_vector(circle* crl1, circle* crl2);
-void check_collision_w_circles(circle_array* arr, int i);
+void set_new_speed_vector(circle* crl1, circle* crl2, double dist);
+void check_collision_w_circles(circle_array* arr, int i, Sound* s);
 void change_selected_circle(circle_array* arr, circle** current_c_ptr, circle* main_c);
+void create_chaos(circle* c);
+void handle_chaos(circle_array* arr);
 int check_ray_collision_w_circle(Vector2 point, const circle_array* col_objs);
+
 
 void handle_move(circle* selected_circle){
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && selected_circle != NULL){
@@ -57,7 +66,7 @@ void handle_add_circle(circle_array* array, float radius, Color color){
 }
 
 void free_circles_if_pressed(circle_array* arr){
-    if (IsKeyPressed(KEY_ENTER)){
+    if (IsKeyPressed(KEY_BACKSPACE)){
         free(arr->circles);
         arr->len = 0;
         arr->capacity = START_CAP;
@@ -88,7 +97,7 @@ void draw_ray(Vector2 start_point, double len, double angle, const circle_array*
 
 void draw_rays(Vector2 start_point, int amount_rays, const circle_array* c, Color color){
     double angle_delta = (2 * PI) / amount_rays;
-    double stop = ((HEIGHT > WIDTH) ? HEIGHT : WIDTH);
+    double stop = RAY_LEN;
     for (int t = 0; t < amount_rays; t++)
         draw_ray(start_point, stop, t * angle_delta, c, color);   
 }
@@ -107,23 +116,27 @@ void add_circle(circle_array* array, Vector2 point, float radius, Color color){
     }
 }
 
-void check_collision_w_border(circle* c){
+void check_collision_w_border(circle* c, Sound* s){
     if ((c->centre.x + c->radius) > WIDTH || (c->centre.x - c->radius) < 0){
-        c->speed.x *= (-1 * 0.7);
+        c->speed.x *= (-1 * BOUNCE_COEFF);
+        if (fabs(c->speed.x) > (fabs(SPEED) / 5)) //MAGIC CONSTANT TO PRPEVENT AWFUL SOUND
+            PlaySound(*s);
     }
     if ((c->centre.y + c->radius) > HEIGHT || (c->centre.y - c->radius) < 0){
-        c->speed.y *= (-1 * 0.7);
+        c->speed.y *= (-1 * BOUNCE_COEFF);
+        if (fabs(c->speed.y) > (fabs(SPEED) / 5)) //MAGIC CONSTANT TO PRPEVENT AWFUL SOUND
+            PlaySound(*s);
     }
 }
 
-void handle_physic(circle_array* arr, circle* selected_circle){
+void handle_physic(circle_array* arr, circle* selected_circle, Sound* s){
     for (int i = 0; i < arr->len; i++)
     {
         if(&(arr->circles[i]) != selected_circle){
-            check_collision_w_circles(arr, i); 
+            check_collision_w_circles(arr, i, s); 
             arr->circles[i].speed = Vector2Add(arr->circles[i].speed, arr->circles[i].acceleration);
             arr->circles[i].centre = Vector2Add(arr->circles[i].speed, arr->circles[i].centre);
-            check_collision_w_border(&(arr->circles[i]));
+            check_collision_w_border(&(arr->circles[i]), s);
             handle_position(arr);
         }
     }
@@ -148,9 +161,8 @@ void handle_position(circle_array* arr){
     }
 }
 
-void set_new_speed_vector(circle* crl1, circle* crl2){
+void set_new_speed_vector(circle* crl1, circle* crl2, double dist){
     float overlap;
-    double dist = Vector2Distance(crl1->centre, crl2->centre);
     Vector2 v1 = crl1->speed;
     Vector2 v2 = crl2->speed;
     Vector2 c1 = crl1->centre;
@@ -173,11 +185,18 @@ void set_new_speed_vector(circle* crl1, circle* crl2){
     }
 }
 
-void check_collision_w_circles(circle_array* arr, int i){
+void check_collision_w_circles(circle_array* arr, int i, Sound* s){
+    double dist;
     for (int j = i; j < arr->len; j++)
     {
-        if (i != j)
-            set_new_speed_vector(&(arr->circles[i]), &(arr->circles[j]));
+        if (i != j){
+            dist = Vector2Distance((arr->circles[i].centre), arr->circles[j].centre);
+            if (dist < (arr->circles[i].radius + arr->circles[j].radius)){
+                set_new_speed_vector(&(arr->circles[i]), &(arr->circles[j]), dist);
+                //if(Vector2Length(arr->circles[i].speed) > (SPEED / 2) || Vector2Length(arr->circles[j].speed) > (SPEED / 2))
+                    //PlaySound(*s);
+            }
+        }   
     }
 }
 
@@ -185,36 +204,62 @@ void change_selected_circle(circle_array* arr, circle** current_c_ptr, circle* m
     if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
         Vector2 mouse_coords = {GetMouseX(), GetMouseY()};
         if (Vector2Distance(mouse_coords, main_c->centre) <= main_c->radius){
-            if (*current_c_ptr != NULL)
-                (*current_c_ptr)->color = RAYWHITE;
+            if (*current_c_ptr != NULL){
+                (*current_c_ptr)->color = MAIN_COLOR;
+                (*current_c_ptr)->radius = RADIUS;
+            }
             *current_c_ptr = main_c;
             (*current_c_ptr)->color = SELECTED_CIRCLE_COLOR;
+            (*current_c_ptr)->radius = SELECTED_CIRCLE_RADIUS;
             return;
         }
         for (int i = 0; i < arr->len; i++)
         {
             if (Vector2Distance(mouse_coords, arr->circles[i].centre) <= arr->circles[i].radius){
-                if (*current_c_ptr != NULL)
-                    (*current_c_ptr)->color = RAYWHITE;
+                if (*current_c_ptr != NULL){
+                    (*current_c_ptr)->color = MAIN_COLOR;
+                    (*current_c_ptr)->radius = RADIUS;
+                }
                 (*current_c_ptr) = &(arr->circles[i]);
                 (*current_c_ptr)->color = SELECTED_CIRCLE_COLOR;
+                (*current_c_ptr)->radius = SELECTED_CIRCLE_RADIUS;
                 return;
             }
         }
     }
     if ((IsMouseButtonReleased(MOUSE_BUTTON_LEFT)))
-        if (*current_c_ptr != NULL)
-            (*current_c_ptr)->color = RAYWHITE;
+        if (*current_c_ptr != NULL){
+            (*current_c_ptr)->color = MAIN_COLOR;
+            (*current_c_ptr)->radius = RADIUS;
+            (*current_c_ptr)->speed.x = 0;
+            (*current_c_ptr)->speed.y = 0;
+        }
         *current_c_ptr = NULL;  
+}
+
+void create_chaos(circle* c){
+    c->speed.x = (rand() % MAX_RANDOM_SPEED - MAX_RANDOM_SPEED / 2);
+    c->speed.y = (rand() % MAX_RANDOM_SPEED - MAX_RANDOM_SPEED / 2);
+}
+
+void handle_chaos(circle_array* arr)
+{
+    if (IsKeyPressed(KEY_ENTER)){
+        for (int i = 0; i < arr->len; i++)
+            create_chaos(&(arr->circles[i]));
+    }
 }
 
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "raylib!");
+    InitAudioDevice();
     SetTargetFPS(70);
 
+    Sound collision_sound = LoadSound("resources/sound_collision.wav");
+
     Vector2 c1Position = {200, 200};
-    circle main_c = {c1Position, RADIUS, RAYWHITE};
+    circle main_c = {c1Position, RADIUS, MAIN_COLOR};
 
     circle* selected_circe = &main_c;
 
@@ -225,23 +270,31 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-        handle_add_circle(&arr, RADIUS, RAYWHITE);
+        handle_add_circle(&arr, RADIUS, MAIN_COLOR);
         handle_move(selected_circe);
+        handle_chaos(&arr);
         change_selected_circle(&arr, &selected_circe, &main_c);
-        handle_physic(&arr, selected_circe);
+        handle_physic(&arr, selected_circe, &collision_sound);
         
         BeginDrawing();
         {
-            ClearBackground(BLACK);
-            draw_rays(main_c.centre, RAYS_AMOUNT, &arr, RAYWHITE);
+            ClearBackground(BACKCOLOR);
+            DrawText("BACKSPACE TO DELETE ALL", 10, 10, 20, GRAY);
+            DrawText("SPACE TO CREATE", 10, 50, 20, GRAY);
+            DrawText("ENTER TO CHAOS", 10, 90, 20, GRAY);
+            DrawText("DRAG & DROP", 10, 130, 20, GRAY);
+            draw_rays(main_c.centre, RAYS_AMOUNT, &arr, MAIN_COLOR);
             DrawCircleV(main_c.centre, main_c.radius, main_c.color);
             for (int i = 0; i < arr.len; i++)
                 DrawCircleV(arr.circles[i].centre, arr.circles[i].radius, arr.circles[i].color);
-
         }
         EndDrawing();  
         free_circles_if_pressed(&arr);
     }
+    UnloadSound(collision_sound);
+
+    CloseAudioDevice();
+
     CloseWindow();
 
     return 0;
